c734331889d295eb66eccf3d521675e732dfd001
serdestonat <onat904@gmail.com>
2024-06-06 18:04:03 +0300

    final commit

--- rsa.py
+++ rsa.py
@@ -12,56 +12,62 @@ def gcd(a, h):
         a = h
         h = temp
 
-p = int(input("p = "))
-q = int(input("q = "))
-n = p * q
-e = int(input("e = "))
-phi = (p - 1) * (q - 1)
+while True:                 # while True ifadesinin
+    p = int(input("p = "))  # konulmasının sebebi p 
+    if p == -1:             # değerine -1 girilene kadar
+        break               # programın arka arkaya çalışması içindir
+    q = int(input("q = "))
+    n = p * q
+    e = int(input("e = "))
+    phi = (p - 1) * (q - 1)
 
-print("n = ", n)
-print("phi = ", phi)
+    print("n = ", n)
+    print("Φ = ", phi)
 
-while gcd(e, phi) != 1:
-    e += 1
+    while gcd(e, phi) != 1:
+        e += 1
 
-# e değeri excel değerinde verildiği için elimizde olan
-# e değerinden hareketle "d*e = 1 mod phi" olacak şekilde d hesaplıyoruz
+    # e değeri excel değerinde verildiği için elimizde olan
+    # e değerinden hareketle "d*e = 1 mod Φ" olacak şekilde d hesaplıyoruz
 
-start_time = time.perf_counter() * 1000  # süre başlangıcı
+    start_time = time.perf_counter() * 1000  # süre başlangıcı 
 
-d = pow(e, -1, phi)
-print("d = ", d)
+    d = pow(e, -1, phi)
+    print("d = ", d)
 
-end_time = time.perf_counter() * 1000  # süre bitişi
-total_time = end_time - start_time  # süre hesabı 
+    end_time = time.perf_counter() * 1000  # süre bitişi
+    total_time = end_time - start_time  # süre hesabı 
 
-print("Gizli Anahtar Oluşturma Süresi : ", total_time, "Milisaniye")  # süre yazdırılması
+    print("Gizli Anahtar Oluşturma Süresi : ", total_time, "Milisaniye")  # süre yazdırılması
 
-# yukarıda verilen süre hesaplarını aşağıda metin şifrelerken ve deşifrelerken de kullanacağız.
+    # süre başlangıç ve bitişlerinin 1000 ile çarpılmasının nedeni sürenin milisaniye cinsinden ölçülecek olmasıdır.
+    # yukarıda verilen süre hesaplarını aşağıda metin şifrelerken ve deşifrelerken de kullanacağız.
 
-# d hesaplama 
+    # d hesaplama 
 
-# Şifrelenecek Mesaj
-msg = int(input("Şifrelenecek Mesaj = "))
+    # Şifrelenecek Mesaj
+    msg = int(input("Şifrelenecek Mesaj = " ))
 
-start_time = time.perf_counter() * 1000
+    start_time = time.perf_counter() * 1000
 
-# Şifreleme c = (msg ^ e) % n
-c = pow(msg, e, n)
-print("Şifrelenmiş Mesaj =", c)
+    # Şifreleme c = (msg ^ e) % n
+    c = pow(msg, e, n)
+    print("Şifrelenmiş Mesaj =", c)
 
-end_time = time.perf_counter() * 1000
-total_time = end_time - start_time
+    end_time = time.perf_counter() * 1000
+    total_time = end_time - start_time
 
-print("Şifreleme Süresi : ", total_time, "Milisaniye")
+    print("Şifreleme Süresi : ", total_time, "Milisaniye")
 
-start_time = time.perf_counter() * 1000
+    start_time = time.perf_counter() * 1000
 
-# Deşifreleme m = (c ^ d) % n
-m = pow(c, d, n)
-print("Gönderilmiş Orijinal Mesaj = ", m)
+    # Deşifreleme m = (c ^ d) % n
+    m = pow(c, d, n)
+    print("Gönderilmiş Orijinal Mesaj =", m)
 
-end_time = time.perf_counter() * 1000
-total_time = end_time - start_time
+    end_time = time.perf_counter() * 1000
+    total_time = end_time - start_time
 
-print("Deşifreleme Süresi : ", total_time, "Milisaniye")
+    print("Deşifreleme Süresi : ", total_time, "Milisaniye")
+
+print("Program sonlandırıldı.")
